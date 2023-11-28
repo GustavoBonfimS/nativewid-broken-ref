71e57ffa6911e3d3037707b5ef7ea5265ff6992b
Gustavo Bonfim <gustavo.hbonfim@outlook.com>
2023-11-28 14:15:21 -0300

    ref working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,38 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, Pressable, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import Animated, { useAnimatedRef } from 'react-native-reanimated';
+import { useRef } from 'react';
 
+const ITEM_COUNT = 100;
+
+const items = Array.from(Array(ITEM_COUNT).keys());
 export default function TabOneScreen() {
+  const listRef = useRef<FlatList>(null);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <FlatList
+        ref={listRef}
+        data={items}
+        keyExtractor={(_, i) => i.toString()}
+        contentContainerStyle={{
+          gap: 20
+        }}
+        renderItem={(_) => (
+          <View style={styles.block} />
+        )}
+        ListFooterComponent={() => (
+          <View style={[styles.block, { backgroundColor: 'yellow'}]} />
+        )}
+      />
+
+      <Pressable style={styles.goEndButton} onPress={() => {
+        listRef?.current?.scrollToEnd?.({
+          animated: true
+        });
+      }} />
     </View>
   );
 }
@@ -16,16 +40,20 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    gap: 30
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  block: {
+    height: 100,
+    width: '100%',
+    backgroundColor: 'purple'
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  goEndButton: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    backgroundColor: 'red'
+  }
 });
